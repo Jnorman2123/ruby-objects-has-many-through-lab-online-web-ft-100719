fe560fcb18440bab62ff78b72048825fc100dd8b
Jonathan Norman <jnorman2123@gmail.com>
2019-10-23 20:16:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,9 @@ class Song
   attr_accessor :name, :artist, :genre
   @@all = []
 
-  def intialize(name)
+  def intialize(name, artist, genre)
     @name = name
+    @artist = artist 
+    @genre = genre
   end
 end
